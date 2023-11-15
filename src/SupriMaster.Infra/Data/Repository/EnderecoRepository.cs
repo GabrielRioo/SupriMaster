09014912f67b25361d09ce548b2be03e851d1f70
GabrielRioo <gabrielpaivario@gmail.com>
2023-11-15 18:54:16 -0300

    feat(:bricks:): create a repository for each entities

---
+++ src/SupriMaster.Infra/Data/Repository/EnderecoRepository.cs
@@ -0,0 +1,15 @@
+﻿using SupriMaster.Business.Models.Fornecedores;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupriMaster.Infra.Data.Repository
+{
+	public class EnderecoRepository : Repository<Endereco>, IEnderecoRepository
+	{
+		public async Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId)
+		{
+			return await ObterPorId(fornecedorId);
+		}
+	}
+}
---
+++ src/SupriMaster.Infra/Data/Repository/FornecedorRepository.cs
@@ -0,0 +1,28 @@
+﻿using SupriMaster.Business.Models.Fornecedores;
+using System;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace SupriMaster.Infra.Data.Repository
+{
+	public class FornecedorRepository : Repository<Fornecedor>, IFornecedorRepository
+	{
+		public async Task<Fornecedor> ObterFornecedorEndereco(Guid id)
+		{
+			return await Db.Fornecedores
+				.AsNoTracking()
+				.Include(f => f.Endereco)
+				.FirstOrDefaultAsync(f => f.Id == id);
+		}
+
+		public async Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id)
+		{
+			return await Db.Fornecedores
+				.AsNoTracking()
+				.Include(f => f.Endereco)
+				.Include(f => f.Produtos)
+				.FirstOrDefaultAsync(f => f.Id == id);
+		}
+
+	}
+}
