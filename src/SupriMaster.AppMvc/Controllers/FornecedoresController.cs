54318063e933998802fd2396df1641da31cd0d33
GabrielRioo <gabrielpaivario@gmail.com>
2023-12-09 19:03:21 -0300

    feat(✨): create the product service and notification methods

---
+++ src/SupriMaster.AppMvc/Controllers/FornecedoresController.cs
@@ -0,0 +1,39 @@
+﻿using SupriMaster.Business.Models.Fornecedores;
+using SupriMaster.Business.Models.Fornecedores.Services;
+using SupriMaster.Infra.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SupriMaster.AppMvc.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+        private readonly IFornecedorService _fornecedorService;
+
+        public FornecedoresController()
+        {
+            _fornecedorService = new FornecedorService(new FornecedorRepository(), new EnderecoRepository());
+        }
+
+        
+        public async Task<ActionResult> Index()
+        {
+            var fornecedor = new Fornecedor()
+            {
+                Nome = "",
+                Documento = "11111",
+                Endereco = new Endereco(),
+                TipoFornecedor = ETipoFornecedor.PessoaFisica,
+                Ativo = true
+            };
+
+            await _fornecedorService.Adicionar(fornecedor);
+
+            return new EmptyResult() ;
+        }
+    }
+}
---
+++ src/SupriMaster.Business/Core/Notificacoes/Notificador.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupriMaster.Business.Core.Notificacoes
+{
+	public class Notificador : INotificador
+	{
+		private List<Notificacao> _notificacoes;
+
+		public Notificador()
+		{
+			_notificacoes = new List<Notificacao>();
+		}
+
+		public void Handle(Notificacao notificacao)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Notificacao> ObterNotificacoes()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool TemNotificacao()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
