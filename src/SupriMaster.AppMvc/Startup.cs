dcb73a912afe2065ccec6edfe96786c2f019416a
GabrielRioo <gabrielpaivario@gmail.com>
2023-12-13 21:36:13 -0300

    chore(🔧): configuration of startup class

--- src/SupriMaster.AppMvc/Startup.cs
+++ src/SupriMaster.AppMvc/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
 
 [assembly: OwinStartupAttribute(typeof(SupriMaster.AppMvc.Startup))]
 namespace SupriMaster.AppMvc
@@ -9,6 +12,11 @@ namespace SupriMaster.AppMvc
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-        }
+
+			AreaRegistration.RegisterAllAreas();
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+		}
     }
 }
